fb780d6cde07150c385019cc91ad23f5da873b35
tomaslago27 <116082698+tomaslago27@users.noreply.github.com>
2025-05-18 21:45:00 -0300

    Cambio en la migracion

---
+++ Migrations/20250519004426_CambioDeDecimalDelTotalVenta.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaVentasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambioDeDecimalDelTotalVenta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Total",
+                table: "Ventas",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Total",
+                table: "Ventas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+    }
+}
